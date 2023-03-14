40fc8f3ed0dfdecbc70e9e65897b9aee335d0109
mdrakibhasanbd369 <mdrakibhasanbd369@gmail.com>
2023-03-15 04:21:47 +0600

    Host fild add

--- logparse.py
+++ logparse.py
@@ -1,6 +1,6 @@
 import re
 import json
-
+from flask import Flask, render_template, request, redirect, url_for
 file = open('ip.log', 'r')
 Lines = file.readlines()
 output_logs = []
@@ -10,7 +10,8 @@ for log_line in Lines:
     count += 1
 # Extract relevant fields using regular expressions
     timestamp = re.findall(r'^(\S+)', log_line)[0]
-    event = re.findall(r'^(?:\S+ )?(\S+)', log_line)[0]
+    host = re.findall(r'^(?:\S+ )?(\S+)', log_line)[0]
+    event = re.findall(r'^(?:\S+ )?(\S+)\ ?(\S+)\:', log_line)[0][1]
     input_interface = re.findall(r'in:(\S+)', log_line)[0]
     output_interface = re.findall(r'out:\((\S+)', log_line)[0]
     src_mac = re.findall(r'src-mac (\S+)', log_line)[0]
@@ -23,6 +24,7 @@ for log_line in Lines:
     # Create dictionary with extracted fields
     log_dict = {
         "Timestamp": timestamp,
+        "Host": host,
         "Event": event,
         "Input Interface": input_interface,
         "Output Interface": output_interface,
@@ -38,7 +40,15 @@ for log_line in Lines:
         "Length": length
     }
     output_logs.append(log_dict)
-
+print(output_logs)
 with open("output.json", "w") as f:
     json.dump(output_logs, f, indent=4)
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return output_logs
+
+if __name__ == '__main__':
+    app.run(debug=True)
